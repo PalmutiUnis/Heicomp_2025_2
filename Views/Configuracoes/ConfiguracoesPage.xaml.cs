05c7c90534709cce49c705edaf57717853e68069
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2025-10-22 14:16:34 -0300

    Adicionando botão voltar

--- Views/Configuracoes/ConfiguracoesPage.xaml.cs
+++ Views/Configuracoes/ConfiguracoesPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class ConfiguracoesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void BotaoVoltarPainelGestao(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//PainelGestaoPage");
+    }
 }
