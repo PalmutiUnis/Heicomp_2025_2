15429fb3b728a7f40d2e80927a8221d9911f89a9
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2025-10-22 19:01:54 -0300

    Modificando cores do ShellTitleView

--- Views/Dashboards/ColaboradoresPage.xaml.cs
+++ Views/Dashboards/ColaboradoresPage.xaml.cs
@@ -2,10 +2,11 @@ namespace Heicomp_2025_2.Views.Dashboards;
 
 public partial class ColaboradoresPage : ContentPage
 {
-	public ColaboradoresPage()
-	{
-		InitializeComponent();
-	}
+    public ColaboradoresPage()
+    {
+        InitializeComponent();
+    }
+
 
     private async void BotaoVoltarPainelGestao(object sender, EventArgs e)
     {
