2a39bb0c6351ceebea571614cb824a32218139d5
João Pedro Tavares Vicente <161774392+Joao-tava@users.noreply.github.com>
2025-10-22 18:39:32 -0300

    Página de Login
    Implementação da Página de Login | Parte visual = completa , Back =Implementar

--- Views/Auth/LoginPage.xaml.cs
+++ Views/Auth/LoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using MauiApp1;
+
 namespace Heicomp_2025_2.Views.Auth;
 
 public partial class LoginPage : ContentPage
@@ -5,5 +7,21 @@ public partial class LoginPage : ContentPage
     public LoginPage()
     {
         InitializeComponent();
+    }
+    private async void OnGoogleLoginClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            //Colocar a API do Google para fazer a autenticação com um simples toque no botão
+            // ... Lógica de autenticação do Google ...
+
+            // Se o login for BEM-SUCEDIDO:
+            // Isso substitui a LoginPage (e sua NavigationPage) pelo AppShell
+            Application.Current.MainPage = new AppShell();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Falha na autenticação com o Google: {ex.Message}", "OK");
+        }
     }
 }
